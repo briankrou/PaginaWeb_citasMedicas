d021efe22649db20f41f9088568720eb06a2beca
Brayan Guevara <bguevaravan@gmail.com>
2023-11-09 22:25:14 -0500

    configuraion boostrap en SaSS

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+/*Aqui las caracteristicas de las diferentes versiones*/
+# v0.1.0
+
+-Esta es la primer version del proyecto citas medicas 
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 // Motor de plantilla
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+app.use(express.static(__dirname+"/public"))
 
   app.get("/", (req, res) => {
     res.render("index", { titulo: "inicio EJS" });
