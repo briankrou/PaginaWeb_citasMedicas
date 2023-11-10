72be4f43bea848d3db7f7f281251d4642632582b
Brayan Guevara <bguevaravan@gmail.com>
2023-11-10 02:42:08 -0500

    se agregan las paginas nosotros servicios contacto y login

--- src/app.js
+++ src/app.js
@@ -13,6 +13,16 @@ app.use(express.static(__dirname+"/public"))
   app.get("/", (req, res) => {
     res.render("index", { titulo: "inicio EJS" });
   });
+  app.get("/nosotros", (req, res) => {
+    res.render("nosotros", { titulo: "inicio EJS" });
+  });
+  app.get("/servicios", (req, res) => {
+    res.render("servicios", { titulo: "inicio EJS" });
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login", { titulo: "inicio EJS" });
+  });
   app.use((req, res, next) => {
     res.status(404).render("404", { titulo: "Página 404" });
   });
