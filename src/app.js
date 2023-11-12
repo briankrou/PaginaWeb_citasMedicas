9bda01e7021e4c48aaac4fb5edda5378d6322557
Brayan Guevara <bguevaravan@gmail.com>
2023-11-12 13:17:03 -0500

    Se agrega  morgan

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 
-const { error } = require('console');
+//const { erro } = require('console');
 const express = require('express')
+const path = require('path');
+const morgan = require('morgan');
 const app = express()
 
 
 const PORT = process.env.PORT || 3000;
+
 
 // Motor de plantilla
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname+"/public"))
 
+//routing
 require('./router/routing')(app);
 
+// middleware
+app.use(morgan('dev'));
 
   app.use((req, res, next) => {
     res.status(404).render("404", { titulo: 'Página 404' });
