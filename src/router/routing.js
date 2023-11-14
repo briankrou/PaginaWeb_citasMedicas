53708751956ee61fc1b7b7a4b8b0564cf7512922
Brayan Guevara <bguevaravan@gmail.com>
2023-11-13 19:19:18 -0500

    Comentarios en routing - endpoints

--- src/router/routing.js
+++ src/router/routing.js
@@ -1,20 +1,27 @@
 
 module.exports = function(app){
 
-      
+  //endpoints
+
+  //endpoint principal
   app.get("/", (req, res) => {
     res.render("index", { titulo: "inicio EJS" });
   });
+
+ //endpoint nosotros
   app.get("/nosotros", (req, res) => {
     res.render("nosotros", { titulo: "inicio EJS" });
   });
+
+   //endpoint servicios
   app.get("/servicios", (req, res) => {
     res.render("servicios", { titulo: "inicio EJS" });
   });
-  
+  //endpoint login
   app.get("/login", (req, res) => {
     res.render("login", { titulo: "inicio EJS" });
   });
+  //enponit usuario
   app.get("/usuario", (req, res) => {
     res.render("usuario", { titulo: "inicio EJS" });
   });
